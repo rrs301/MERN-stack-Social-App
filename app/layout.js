e498557618f3a32371979115495d199f1ea72a56
rrs301 <tubegurji007@gmail.com>
2024-01-03 19:36:43 -0500

    Starter Pack

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Outfit } from 'next/font/google'
 import './globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
+
 
 const inter = Outfit({ subsets: ['latin'] })
 
@@ -11,10 +11,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+  
     <html lang="en">
       <body className={inter.className} >{children}</body>
     </html>
-    </ClerkProvider>
+
   )
 }
